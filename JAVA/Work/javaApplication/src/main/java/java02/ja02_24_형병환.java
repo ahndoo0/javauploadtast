250583d9f6e892b0dd03ac6ad73ff777e584ab23
ahndoo0 <eh_127@naver.com>
2017-09-18 12:05:51 +0900

    java

--- JAVA/Work/javaApplication/src/main/java/java02/ja02_24_형병환.java
+++ JAVA/Work/javaApplication/src/main/java/java02/ja02_24_형병환.java
@@ -9,16 +9,16 @@ public class ja02_24_형병환 {
         int b='2'+(true?1:0);
         System.out.println(b);
         
-        int c=Integer.parseInt("15")-5;
+        int c=Integer.parseInt("15")+5;
         System.out.println(c);
         
-        int d="1"+'2' ;
+        int d=Integer.parseInt("1")+'2' ;
         System.out.println(d);
         
-        int e=false+true;
+        int e=(false?1:0)+(true?1:0);
         System.out.println(e);
        
-        // 형변환은 정수 실수 문자만 가능!
+        // 형변환은 정수 실수 문자 만 가능!
         
     }
 }
