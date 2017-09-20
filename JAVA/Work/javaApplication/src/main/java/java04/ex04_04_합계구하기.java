5d50ce9cdad1b698c2bba606fb1be11cc40a521e
ahndoo0 <eh_127@naver.com>
2017-09-20 18:10:28 +0900

    java

--- JAVA/Work/javaApplication/src/main/java/java04/ex04_04_합계구하기.java
+++ JAVA/Work/javaApplication/src/main/java/java04/ex04_04_합계구하기.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class ex04_04_합계구하기 {
     public static void main(String[] args){
         
-        int i,j,z;
+        int i;
         int sum=0;
         
         Scanner keboard =new Scanner(System.in);
+        
         System.out.print("시작값을 입력하시오");
-        z = keboard.nextInt();
+        int z = keboard.nextInt();
        
         System.out.print("종료값을 입력하시오");
-        j = keboard.nextInt();
+        int j = keboard.nextInt();
        
         if(z>j){
             int t=z;
@@ -22,7 +23,7 @@ public class ex04_04_합계구하기 {
         }
         
         for(i=z; i<=j; i=i+1){
-            sum = sum+i;
+             sum = sum+i;
              
     }
         System.out.println(z+"부터"+j+"까지의 합은"+sum+"입니다");
