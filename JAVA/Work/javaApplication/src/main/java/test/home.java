10165b63d7cd87bc95aa59b9dc92f1de08fdbd5f
ahndoo0 <eh_127@naver.com>
2017-09-22 01:06:32 +0900

    jv

--- JAVA/Work/javaApplication/src/main/java/test/home.java
+++ JAVA/Work/javaApplication/src/main/java/test/home.java
@@ -1,12 +1,29 @@
 package test;
 
+import java.util.Scanner;
 
 public class home {
-    public static void main(System[] args) {
-        int x=10;
-        int y=20;
-        System.out.println(x>y);
-        
+    public static void main(String[] args) {
+       
+        Scanner keboerd = new Scanner(System.in);
+       
+        System.out.print("시작값 :");
+        int x = keboerd.nextInt();
+        System.out.print("종료값 :");
+        int y = keboerd.nextInt();
+    
+        if(x>y){
+        System.out.println(y);
+        int temp;
+        temp=y;
+        x=y;
+        x=temp;
+        }
+        int sum=0;
+        for(int i=x; i<=y; i=i+1){
+        sum = sum+i;
+        System.out.println(sum);
+        }
     }
 
 }
