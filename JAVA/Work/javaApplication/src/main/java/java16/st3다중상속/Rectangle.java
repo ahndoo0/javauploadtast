0f90f74ac3b22dcbc5b0fd962f82b18ed09d346e
ahndoo0 <eh_127@naver.com>
2017-10-19 18:12:44 +0900

    dsadasda

---
+++ JAVA/Work/javaApplication/src/main/java/java16/st3다중상속/Rectangle.java
@@ -0,0 +1,15 @@
+package java16.st3다중상속;
+
+import java16.st1abstract.Shape;
+
+public class Rectangle extends java16.st3다중상속.Shape implements Drawable {
+    public int w,h;
+    
+    
+    @Override
+    public void draw() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
