86083e2f690447ea655c8ba739f2e5409bddc398
ahndoo0 <eh_127@naver.com>
2017-10-20 18:17:29 +0900

    sdsdsdsd

--- JAVA/Work/javaApplication/src/main/java/java15/st4anonyomous/TV.java
+++ JAVA/Work/javaApplication/src/main/java/java15/st4anonyomous/TV.java
@@ -4,25 +4,21 @@ public class TV implements RemoteControl {
     
     @Override
     public void turnOn() {
-        // TODO Auto-generated method stub
         
     }
     
     @Override
     public void turnOff() {
-        // TODO Auto-generated method stub
         
     }
     
     @Override
     public void moveLeft() {
-        // TODO Auto-generated method stub
         
     }
     
     @Override
     public void moveRight() {
-        // TODO Auto-generated method stub
         
     }
     
---
+++ JAVA/Work/javaApplication/src/main/java/java17/st1shape/ShapeTest1.java
@@ -0,0 +1,17 @@
+package java17.st1shape;
+
+public class ShapeTest1 {
+    public static void main(String[] args) {
+    Circle c = new Circle();
+    c.draw(); //Circle draw()
+    
+    Rectangle r = new Rectangle();
+    r.draw();//Rectangle draw()
+    
+    Triangle t = new Triangle();
+    t.draw();//Triangle draw()
+    
+    Shape s = new Rectangle();
+    s.draw();
+    }
+}
