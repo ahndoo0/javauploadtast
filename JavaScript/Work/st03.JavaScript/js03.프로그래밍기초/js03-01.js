751a2f06076d0147692064525c1a70fd1adda375
ahndoo0 <eh_127@naver.com>
2017-11-20 10:37:47 +0900

    kkkkkk

--- JavaScript/Work/st03.JavaScript/js03.프로그래밍기초/js03-01.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2016-07-10.
- */
-"use strict";
-
-var i = 10;
-var I = 10;
-
-
--- JavaScript/Work/st05.jQuery/jq10.Ajax메서드/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-function call(param){
-    return ("Hello, " + param);
-}
