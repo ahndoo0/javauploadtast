4c9e16230a72862becf305972664c2742e724a72
ahndoo0 <eh_127@naver.com>
2017-12-05 18:06:44 +0900

    asdasd

---
+++ Android/Work/part1.widget/ad01-03.BaseApp/app/src/main/java/com/android/baseapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.android.baseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 =(Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),R.string.btnclick,Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+}
