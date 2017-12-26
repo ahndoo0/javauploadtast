dbda9de8ee9c16d50e3d697d166a6acccb731c00
ahndoo0 <eh_127@naver.com>
2017-12-26 15:31:04 +0900

    000000

---
+++ Spring/Work/spring11.di/src/main/java/di01/model/Employee.java
@@ -0,0 +1,59 @@
+package di01.model;
+
+public class Employee {
+    private String 이름;
+    private String 주소;
+    private int    월급;
+    private String 주민번호;
+  
+    public Employee() {
+        super();
+    }
+    
+    public Employee(String 이름, String 주소, int 월급, String 주민번호) {
+        super();
+        this.이름 = 이름;
+        this.주소 = 주소;
+        this.월급 = 월급;
+        this.주민번호 = 주민번호;
+    }
+    
+    @Override
+    public String toString() {
+        return "Employee [이름=" + 이름 + ", 주소=" + 주소 + ", 월급=" + 월급 + ", 주민번호="
+                + 주민번호 + "]";
+    }
+    
+    public String get이름() {
+        return 이름;
+    }
+    
+    public void set이름(String 이름) {
+        this.이름 = 이름;
+    }
+    
+    public String get주소() {
+        return 주소;
+    }
+    
+    public void set주소(String 주소) {
+        this.주소 = 주소;
+    }
+    
+    public int get월급() {
+        return 월급;
+    }
+    
+    public void set월급(int 월급) {
+        this.월급 = 월급;
+    }
+    
+    public String get주민번호() {
+        return 주민번호;
+    }
+    
+    public void set주민번호(String 주민번호) {
+        this.주민번호 = 주민번호;
+    }
+    
+}
